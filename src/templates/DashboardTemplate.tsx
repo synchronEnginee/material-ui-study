d3f5de4be2938243bab9fbc7f2ed5f18af96e7f1
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-04-09 18:06:50 +0900

    From material-ui(v4) To v5 and sidebar header

--- src/templates/DashboardTemplate.tsx
+++ src/templates/DashboardTemplate.tsx
@@ -1,19 +1,95 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Container from '@material-ui/core/Container';
+import { styled, useTheme, CSSObject, Theme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import MenuIcon from '@mui/icons-material/Menu';
+// ハンバーガーメニュー開閉のためのボタン
+import IconButton from '@mui/material/IconButton';
 
-import { makeStyles, Theme } from '@material-ui/core/styles';
+// サイドバーの幅をヘッダーで考慮するため
+const drawerWidth = 240;
 
 // useStyles を追加してスタイルをカスタマイズ
-const useStyles = makeStyles((theme: Theme) => ({
-  toolbar: {
-    fontWeight: 'bold',
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
   },
-  link: {
-    margin: theme.spacing(1, 1.5),
-  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
 }));
 
 export interface DashboardTemplateProps {
@@ -25,27 +101,63 @@ const DashboardTemplate: React.FC<DashboardTemplateProps> = ({
   children,
   title,
 }) => {
-  const classes = useStyles();
+  // material-uiのthemeをJSX内から参照するため
+  const theme = useTheme();
+  // drawerの開閉状態管理
+  const [open, setOpen] = React.useState(false);
 
+  // drawerの開閉
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
   return (
-    <>
-      <AppBar>
-        <Toolbar className={classes.toolbar}>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            onClick={handleDrawerToggle}
+            sx={{ marginRight: 5, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
           <nav>
-            <Link to="/" className={classes.link}>
-              Top
-            </Link>
-            <Link to="/about" className={classes.link}>
-              About
-            </Link>
+            <Link to="/">Top</Link>
+            <Link to="/about">About</Link>
           </nav>
         </Toolbar>
       </AppBar>
+      <Drawer variant="permanent" open={open} onClose={handleDrawerToggle}>
+        <DrawerHeader>
+          {/* 向きが変わるとIconボタンを変更 */}
+          <IconButton onClick={handleDrawerToggle}>
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <InfoIcon />
+            </ListItemIcon>
+            <ListItemText primary="About" secondary="hogehoge" />
+          </ListItem>
+        </List>
+      </Drawer>
       <Container>
         <h1>{title}</h1>
         <div>{children}</div>
       </Container>
-    </>
+    </Box>
   );
 };
 
